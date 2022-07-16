7cba4a1d32422ab011f320d11d74267ef57714ae
Thais-Mont <thais.vmonteiro@gmail.com>
2022-07-15 21:20:30 -0300

    ajustes jwt

--- src/users/user.route.js
+++ src/users/user.route.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
 const userController = require('./user.controller');
-const authMiddleware = require("../auth/auth.middleware")
 
 router.post('/create', userController.createUserController);
-router.get('/', authMiddleware, userController.findAllUserController);
+router.get('/', userController.findAllUserController);
 
 module.exports = router;
