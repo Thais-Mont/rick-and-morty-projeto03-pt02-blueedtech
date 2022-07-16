996a5ea2ff32bdd687d0ef68cfebadfedfe66f10
Thais-Mont <thais.vmonteiro@gmail.com>
2022-07-15 21:29:23 -0300

    user auth

--- src/users/user.route.js
+++ src/users/user.route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const userController = require('./user.controller');
 
 router.post('/create', userController.createUserController);
-router.get('/', userController.findAllUserController);
+router.get('/', authMiddleware, userController.findAllUserController);
 
 module.exports = router;
